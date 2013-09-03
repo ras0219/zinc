d8961baf3e5a7d149a1ad55ac6213e2e71a8a321
Robert Schumacher <robertallenschumacher@gmail.com>
2013-09-03 15:11:18 -0500

    Updated snacks plugin

--- plugins/snacks/snacks_plugin.cpp
+++ plugins/snacks/snacks_plugin.cpp
@@ -1,9 +1,10 @@
 #include <cassert>
 #include <zinc_plugin>
 #include <vector>
+#include <zinc_interactive>
 
 using namespace std;
-using namespace pnp;
+using namespace zinc;
 
 namespace SnacksPlugin
 {
@@ -13,45 +14,58 @@ namespace SnacksPlugin
 
   static unsigned int snacks = 2;
 
-  void snacktime(Context* ctx, str_t) {
+  void snacktime(OStream::Interface* ctx, str_t) {
     if (snacks < 2) {
-      ctx->vtable->reply(ctx, "Sorry, I don't have enough snacks.");
+      ctx->vtable->send(ctx, "Sorry, I don't have enough snacks.");
       return;
     } else {
       snacks -= 2;
     }
       
-    ctx->vtable->reply(ctx, botsnacks[rand() % botsnacks.size()]);
-    ctx->vtable->reply(ctx, botsnacks[rand() % botsnacks.size()]);
+    ctx->vtable->send(ctx, botsnacks[rand() % botsnacks.size()]);
+    ctx->vtable->send(ctx, botsnacks[rand() % botsnacks.size()]);
   }
 
-  void botsnack(Context* ctx, str_t) {
+  void botsnack(OStream::Interface* ctx, str_t) {
     snacks++;
     std::stringstream ss;
     ss << "Thanks for the snack! I now have " << snacks << " snack";
     if (snacks > 1) ss << "s."; else ss << ".";
-    ctx->vtable->reply(ctx, ss.str().c_str());
+    ctx->vtable->send(ctx, ss.str().c_str());
     return;
   }
 
-  void install(PluginHost* host) {
-    host->vtable->register_command(host, "snacktime", &snacktime);
-    host->vtable->register_command(host, "botsnack", &botsnack);
+  struct InstallInterfaces {
+    Interactive::Interface* i;
+  };
+
+  void install(Interface** argv) {
+    InstallInterfaces* ii = (InstallInterfaces*)(argv);
+    auto i = ii->i;
+    i->vtable->register_command(i, "snacktime", &snacktime);
+    i->vtable->register_command(i, "botsnack", &botsnack);
   }
 
   static str_t plugin_name = "SnacksMachine";
-  static SemanticVersion plugin_version = { 0, 0, 0 };
+  static SemanticVersion plugin_version = { 0, 1, 0 };
 
-  static PluginBase plugbase = {
+  InterfaceRequest requests[] = {
+    Interactive::Request::required
+  };
+
+  static Plugin plugbase = {
     &install,
     nullptr, // uninstall
     nullptr, // transfer
     plugin_name,
-    plugin_version
+    plugin_version,
+
+    1,
+    requests
   };
 };
 
-PluginBase* get_plugin(std::size_t n) {
+Plugin* get_plugin(std::size_t n) {
   assert(n == 0);
   return &SnacksPlugin::plugbase;
 }
@@ -59,9 +73,6 @@ PluginBase* get_plugin(std::size_t n) {
 pnp_module_t pnp_module =
 {
   CUR_ZINC_VERSION,             // req_zinc_version
-  nullptr,                      // init
-  nullptr,                      // destroy
-
   1,                            // num_exported_plugins
   get_plugin                    // get_plugin
 };
